76007938b55524a3677bee05e399add1b2294b51
Faraz <farazmalik52@gmail.com>
2018-10-09 15:48:24 +0500

    Admin Login Working

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+
+/////////////////////////////////////////////
+
+
+    Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+    Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+    Route::post('/admin/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+    Route::post('/users/logout', 'Auth\LoginController@userLogout')->name('users.logout');
+
+
+
+
